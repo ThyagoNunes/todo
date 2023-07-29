05cb77161ce31a49c3247e756181299d301be1d2
thyago nunes <devthyagonunes@gmail.com>
2023-07-29 20:42:25 -0300

    feat: backButton

--- src/components/TaskDetails/index.jsx
+++ src/components/TaskDetails/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import Button from "../Button";
 
@@ -7,10 +8,15 @@ import "./styles.css";
 
 function TaskDetails() {
   const params = useParams();
+  const navigate = useNavigate();
+
+  function handleBackButtonClick() {
+    navigate(`/`);
+  }
   return (
     <>
       <div className="back-button-container">
-        <Button>Voltar</Button>
+        <Button onClick={handleBackButtonClick}>Voltar</Button>
       </div>
       <div className="task-details-container">
         <h2>{params.taskTitle}</h2>
