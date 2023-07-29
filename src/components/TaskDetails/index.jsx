654fe0b7413ccf7319b4aeed430ab4a11ad66bc0
thyago nunes <devthyagonunes@gmail.com>
2023-07-29 20:29:01 -0300

    feat: create and styled component taskdetails

---
+++ src/components/TaskDetails/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+import Button from "../Button";
+
+import "./styles.css";
+
+function TaskDetails() {
+  const params = useParams();
+  return (
+    <>
+      <div className="back-button-container">
+        <Button>Voltar</Button>
+      </div>
+      <div className="task-details-container">
+        <h2>{params.taskTitle}</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
+          repellat ratione dolorem ut perferendis, obcaecati tenetur ab unde
+          harum neque beatae, rem dolore, optio adipisci sint tempore ipsam est
+          nihil.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default TaskDetails;
