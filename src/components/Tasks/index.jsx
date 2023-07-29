fd433e1c33716c1983c773af6bf9d422f8d2095b
thyago nunes <devthyagonunes@gmail.com>
2023-07-29 17:53:04 -0300

    feat: propdrilling handleTaskClick

--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 import Task from "../Task";
 
-function Tasks({ tasks }) {
+function Tasks({ tasks, handleTaskClick }) {
   return (
     <>
       {tasks.map((task) => (
-        <Task task={task} />
+        <Task task={task} handleTaskClick={handleTaskClick} />
       ))}
     </>
   );
